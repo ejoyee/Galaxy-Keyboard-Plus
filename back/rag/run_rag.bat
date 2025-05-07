bdf0731eadfd2fefbca051715f2e20d551745a8a
김다정 <rlaekwjd6566@gmail.com>
2025-05-07 10:16:29 +0900

    fix: requirements.txt 경로 수정 && 배치 파일 수정 && 포트 변경

--- back/rag/run_rag.bat
+++ back/rag/run_rag.bat
@@ -5,24 +5,22 @@ echo 현재 작업 디렉토리 확인:
 cd
 
 :: 가상환경 생성
-if not exist "app\venv" (
+if not exist "venv" (
     echo [🔧] 가상환경 생성 중...
-    python -m venv app\venv
+    python -m venv venv
 )
 
 :: 가상환경 활성화
 echo [🚀] 가상환경 활성화...
-call app\venv\Scripts\activate
+call venv\Scripts\activate
 
 :: 의존성 설치
 echo [📦] requirements 설치 중...
-cd app
 pip install -r requirements.txt
-cd ..
 
 :: 서버 실행
 echo [🔥] FastAPI 서버 실행 중...
-python -m uvicorn app.main:app --reload --port 9000
+python -m uvicorn app.main:app --reload --port 8090
 
 endlocal
 pause
